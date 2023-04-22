10cc89d4f2d4013e6178604e186592ba72375ced
Саша <friend_adversary@mail.ru>
2023-04-22 17:42:25 +0300

    Second Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.surname = "Иванов";
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.passport = "4444 № 44444444";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
 }
